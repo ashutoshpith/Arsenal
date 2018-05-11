c2e710018f356683822120ae69eda47a0856e5ea
ashutosh <amxdp18@gmail.com>
2018-05-11 19:16:01 +0530

    added docs controller and initalize model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+  resources :docs
+
+  get 'docs/index'
+
+  get 'docs/edit'
+
+  get 'docs/new'
+
+  get 'docs/show'
+
+root 'docs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
